13adab2c0bdf0f3c743c78b4cffc0f8cb23e05cf
Erik Lindgren <erik.lindgren@prototyp.se>
2021-09-16 11:31:17 +0200

    feat: Add tests (#39)
    * Add test for generate command
    
    * Add test for migrate --force flag
    
    * Add .nvmrc, update lockfile
    
    * Add 'run tests' github action
    
    Co-authored-by: Sanna Marklund <sanna.marklund@prototyp.se>

---
+++ __tests__/generate.test.js
@@ -0,0 +1,17 @@
+const execa = require('execa')
+const fs = require('fs')
+const { join } = require('path')
+
+describe('generate', () => {
+    it('should create a new migration file', async () => {
+        const migrationName = 'test-migration'
+        const { stdout } = await execa('cmp', ['generate', migrationName])
+
+        const migrationFileName = stdout.split(' ').find((m) => m.indexOf('js') > -1)
+        const migrationFile = fs.readFileSync(`${process.env.MIGRATIONS_DIR}/${migrationFileName}`, 'utf-8')
+
+        const template = fs.readFileSync(join(__dirname, '..', 'templates', 'migration.mustache'), 'utf8')
+
+        expect(migrationFile).toEqual(template)
+    })
+})
