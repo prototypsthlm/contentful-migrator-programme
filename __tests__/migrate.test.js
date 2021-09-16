aad600644a7b637ebb2fc9d4737368c7c58d7caf
Erik Lindgren <erik.lindgren@prototyp.se>
2021-09-16 11:05:55 +0200

    Add 'run tests' github action

--- __tests__/migrate.test.js
+++ __tests__/migrate.test.js
@@ -2,8 +2,8 @@ const execa = require('execa')
 
 describe('migrate', () => {
     it('should demand user to use "--force" flag if running against master space', async () => {
-        process.env.CTF_ENVIRONMENT = 'master'
-        const { stderr } = await execa('cmp', ['migrate'])
-        expect(stderr).toEqual('Executing migrations against master requires the --force flag.')
+        process.env.CTF_ENVIRONMENT_ID = 'master'
+        const { stdout } = await execa('cmp', ['migrate'])
+        expect(stdout).toMatch('Executing migrations against master requires the --force flag.')
     })
 })
